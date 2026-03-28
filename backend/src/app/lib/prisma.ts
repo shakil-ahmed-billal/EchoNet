07bf1f142576d307df7fe6e3b33b308d4fdef879
shakil-ahmed-billal <itzxhakil@gmail.com>
2026-03-28 06:12:27 +0600

    fix(db): define accelerateUrl in PrismaClient options explicitly for Prisma 7 Edge Client initialization

--- backend/src/app/lib/prisma.ts
+++ backend/src/app/lib/prisma.ts
@@ -1,7 +1,11 @@
 import "dotenv/config";
 import { PrismaClient } from "../../../generated/prisma/client/index.js";
+import config from '../config/index.js';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  accelerateUrl: config.database_url,
+  log: ['info', 'warn', 'error'],
+});
 
 export default prisma;
 export { prisma };
